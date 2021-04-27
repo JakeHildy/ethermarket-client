c17602e6060afbf1163d9368516fd54be8fb8ee1
Jacob Hildebrandt <jake@hildy.ca>
2021-04-27 12:58:14 -0700

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles/global.scss";
+import NavBar from "./components/molecules/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>EtherMarket</h1>
+        <Router>
+          <NavBar />
+          <h1>ROUTER</h1>
+        </Router>
       </div>
     );
   }
---
+++ src/components/molecules/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./NavBar.scss";
+import { Link } from "react-router-dom";
+import ProfileIcon from "./../../../assets/icons/account_circle_black_24dp.svg";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <Link to="/browse" className="navbar__browse-link">
+        <h2 className="navbar__home-text">EtherMarket</h2>
+      </Link>
+      <Link to="/home" className="navbar__home-link">
+        <img
+          src={ProfileIcon}
+          alt="Navbar Profile Icon"
+          className="navbar__profile-icon"
+        />
+      </Link>
+    </nav>
+  );
+}
+
+export default NavBar;
