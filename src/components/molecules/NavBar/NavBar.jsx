1a767f9e5d98df2c3378664f9684822e72383022
Jacob Hildebrandt <jake@hildy.ca>
2021-04-27 17:58:04 -0700

    Basic Browse Styling for Desktop

--- src/components/molecules/NavBar/NavBar.jsx
+++ src/components/molecules/NavBar/NavBar.jsx
@@ -6,16 +6,18 @@ import ProfileIcon from "./../../../assets/icons/account_circle_black_24dp.svg";
 function NavBar() {
   return (
     <nav className="navbar">
-      <Link to="/browse" className="navbar__browse-link">
-        <h2 className="navbar__home-text">EtherMarket</h2>
-      </Link>
-      <Link to="/home" className="navbar__home-link">
-        <img
-          src={ProfileIcon}
-          alt="Navbar Profile Icon"
-          className="navbar__profile-icon"
-        />
-      </Link>
+      <div className="navbar__container">
+        <Link to="/browse" className="navbar__browse-link">
+          <h2 className="navbar__home-text">EtherMarket</h2>
+        </Link>
+        <Link to="/home" className="navbar__home-link">
+          <img
+            src={ProfileIcon}
+            alt="Navbar Profile Icon"
+            className="navbar__profile-icon"
+          />
+        </Link>
+      </div>
     </nav>
   );
 }
