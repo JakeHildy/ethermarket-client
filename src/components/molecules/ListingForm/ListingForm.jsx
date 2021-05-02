5a877e699f549cc0b02e9cf09be8285b20c993d9
Jacob Hildebrandt <jake@hildy.ca>
2021-05-02 11:35:03 -0700

    Refactored out ListingForm from EditListingPage

---
+++ src/components/molecules/ListingForm/ListingForm.jsx
@@ -0,0 +1,161 @@
+import React, { Component } from 'react';
+import './ListingForm.scss';
+import InputField from './../../atoms/InputField/InputField';
+import TextArea from './../../atoms/TextArea/TextArea';
+import DropDownField from './../../atoms/DropDownField/DropDownField';
+import ButtonPrimary from './../../atoms/ButtonPrimary/ButtonPrimary';
+import ButtonSecondary from './../../atoms/ButtonSecondary/ButtonSecondary';
+import ButtonDanger from './../../atoms/ButtonDanger/ButtonDanger';
+
+export class ListingForm extends Component {
+  state = {
+    title: '',
+    price: '',
+    listCurrency: 'ETH',
+    category: 'Miscellaneous',
+    condition: '',
+    location: '',
+    description: '',
+    titleError: '',
+    priceError: '',
+    descriptionError: '',
+  };
+
+  componentDidMount() {
+    const { title, price, listCurrency, category, condition, location, description } = this.props.listing;
+    this.setState({ title, price, listCurrency, category, condition, location, description });
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  attemptSubmit = (e) => {
+    e.preventDefault();
+    const { handleSubmit } = this.props;
+
+    // Validation
+    let titleError = '';
+    if (!this.state.title) {
+      titleError = 'Title Required';
+    }
+    let priceError = '';
+    if (!this.state.price) {
+      priceError = 'Price Required';
+    }
+    if (this.state.price < 0) {
+      priceError = "Price can't be negative";
+    }
+    if (!Number(this.state.price)) {
+      priceError = 'Enter valid number';
+    }
+    let descriptionError = '';
+    if (!this.state.description) {
+      descriptionError = 'Description Required';
+    }
+
+    this.setState(
+      {
+        titleError,
+        priceError,
+        descriptionError,
+      },
+      () => {
+        if (titleError || priceError || descriptionError) {
+          return;
+        }
+        const { title, price, listCurrency, category, condition, location, description } = this.state;
+        const data = { title, price, listCurrency, category, condition, location, description };
+        handleSubmit(data);
+      }
+    );
+  };
+
+  render() {
+    const { handleCancel, handleDelete } = this.props;
+    return (
+      <div className="listing-form__container-bottom">
+        <div className="listing-form__container-bottom-left">
+          <InputField
+            name="title"
+            label="Title *"
+            value={this.state.title}
+            placeholder="Enter Title..."
+            onChange={this.handleChange}
+            error={this.state.titleError}
+          />
+          <div className="listing-form__currency-input">
+            <div className="listing-form__amount">
+              <InputField
+                name="price"
+                label="Price *"
+                value={this.state.price}
+                placeholder="Enter Price..."
+                onChange={this.handleChange}
+                error={this.state.priceError}
+              />
+            </div>
+            <div className="listing-form__currency">
+              <DropDownField
+                name="listCurrency"
+                label="Currency"
+                options={['ETH', 'BTC']}
+                value={this.state.listCurrency}
+                onChange={this.handleChange}
+                error=""
+              />
+            </div>
+          </div>
+          <DropDownField
+            name="category"
+            label="Category"
+            options={['Miscellaneous', 'Home', 'Clothing', 'Electronics', 'Hobbies', 'Entertainment', 'Sporting']}
+            value={this.state.category}
+            onChange={this.handleChange}
+            error=""
+          />
+          <InputField
+            name="condition"
+            label="Condition"
+            value={this.state.condition}
+            placeholder="Enter Condition..."
+            onChange={this.handleChange}
+            error=""
+          />
+        </div>
+        <div className="listing-form__container-bottom-right">
+          <InputField
+            name="location"
+            label="Location"
+            value={this.state.location}
+            placeholder="Enter Postal Code..."
+            onChange={this.handleChange}
+            error=""
+          />
+          <TextArea
+            name="description"
+            label="Description *"
+            value={this.state.description}
+            placeholder="Enter Description..."
+            onChange={this.handleChange}
+            error={this.state.descriptionError}
+          />
+          <div className="listing-form__buttons">
+            <div className="listing-form__button-cancel">
+              <ButtonSecondary label="Cancel" handleClick={handleCancel} />
+            </div>
+            <div className="listing-form__button-cancel">
+              <ButtonDanger label="Delete" handleClick={handleDelete} />
+            </div>
+            <div className="listing-form__button-create">
+              <ButtonPrimary label="Submit" handleClick={this.attemptSubmit} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ListingForm;
--- src/pages/EditListingPage/EditListingPage.jsx
+++ src/pages/EditListingPage/EditListingPage.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import './EditListingPage.scss';
-import InputField from './../../components/atoms/InputField/InputField';
-import TextArea from './../../components/atoms/TextArea/TextArea';
-import DropDownField from './../../components/atoms/DropDownField/DropDownField';
-import ButtonPrimary from './../../components/atoms/ButtonPrimary/ButtonPrimary';
-import ButtonSecondary from './../../components/atoms/ButtonSecondary/ButtonSecondary';
-import ButtonDanger from './../../components/atoms/ButtonDanger/ButtonDanger';
+import ListingForm from './../../components/molecules/ListingForm/ListingForm';
 import AddPicsIcon from './../../assets/icons/add_photo_alternate_black_24dp.svg';
 import DeleteIcon from './../../assets/icons/delete_forever_white_24dp.svg';
 import axios from 'axios';
@@ -13,18 +8,18 @@ const { v4: uuidv4 } = require('uuid');
 
 export class EditListingPage extends Component {
   state = {
-    _id: '',
-    title: '',
-    titleError: '',
-    price: '',
-    priceError: '',
-    listCurrency: 'ETH',
-    category: 'Miscellaneous',
-    condition: '',
-    location: '',
-    description: '',
-    descriptionError: '',
-    images: [],
+    listing: {
+      _id: '',
+      title: '',
+      price: '',
+      listCurrency: 'ETH',
+      category: 'Miscellaneous',
+      condition: '',
+      location: '',
+      description: '',
+      images: [],
+    },
+    listingLoaded: false,
     selectedFile: null,
     userId: '123456',
   };
@@ -44,22 +39,10 @@ export class EditListingPage extends Component {
         images,
       } = res.data.data.listing;
       this.setState({
-        _id,
-        title,
-        price,
-        listCurrency,
-        category,
-        condition,
-        location,
-        description,
-        images,
+        listing: { _id, title, price, listCurrency, category, condition, location, description, images },
+        listingLoaded: true,
       });
     });
-  };
-
-  handleChange = (e) => {
-    e.preventDefault();
-    this.setState({ [e.target.name]: e.target.value });
   };
 
   onPhotoChangeHandler = (e) => {
@@ -79,62 +62,29 @@ export class EditListingPage extends Component {
     });
   };
 
-  handleEditListing = (e) => {
-    e.preventDefault();
-
-    // Validation
-    let titleError = '';
-    if (!this.state.title) {
-      titleError = 'Title Required';
-    }
-    let priceError = '';
-    if (!this.state.price) {
-      priceError = 'Price Required';
-    }
-    if (this.state.price < 0) {
-      priceError = "Price can't be negative";
-    }
-    if (!Number(this.state.price)) {
-      priceError = 'Enter valid number';
-    }
-    let descriptionError = '';
-    if (!this.state.description) {
-      descriptionError = 'Description Required';
-    }
-
-    this.setState(
-      {
-        titleError,
-        priceError,
-        descriptionError,
-      },
-      () => {
-        if (titleError || priceError || descriptionError) {
-          return;
-        }
-        axios
-          .patch(`${process.env.REACT_APP_BACKEND_EP}${process.env.REACT_APP_LISTINGS_EP}/${this.state._id}`, {
-            creatorId: this.state.userId,
-            posted: true,
-            sold: false,
-            title: this.state.title,
-            price: this.state.price,
-            listCurrency: this.state.listCurrency,
-            category: this.state.category,
-            condition: this.state.condition,
-            location: { lat: '41.40338', long: '2.17403' },
-            description: this.state.description,
-            images: this.state.images,
-            followers: [],
-          })
-          .then((res) => {
-            console.log(res);
-          })
-          .catch((err) => {
-            console.log(`💣 === ERROR PATCHING LISTING === 💣`, err);
-          });
-      }
-    );
+  handleEditListing = (data) => {
+    console.log(data);
+    axios
+      .patch(`${process.env.REACT_APP_BACKEND_EP}${process.env.REACT_APP_LISTINGS_EP}/${this.state.listing._id}`, {
+        creatorId: this.state.userId,
+        posted: true,
+        sold: false,
+        title: data.title,
+        price: data.price,
+        listCurrency: data.listCurrency,
+        category: data.category,
+        condition: data.condition,
+        location: { lat: '41.40338', long: '2.17403' },
+        description: data.description,
+        images: this.state.images,
+        followers: [],
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(`💣 === ERROR PATCHING LISTING === 💣`, err);
+      });
   };
 
   handleCancel = (e) => {
@@ -153,7 +103,7 @@ export class EditListingPage extends Component {
   handleDeleteListing = (e) => {
     e.preventDefault();
     axios
-      .delete(`${process.env.REACT_APP_BACKEND_EP}${process.env.REACT_APP_LISTINGS_EP}/${this.state._id}`)
+      .delete(`${process.env.REACT_APP_BACKEND_EP}${process.env.REACT_APP_LISTINGS_EP}/${this.state.listing._id}`)
       .then((res) => {
         console.log(res);
       })
@@ -166,6 +116,7 @@ export class EditListingPage extends Component {
   };
 
   render() {
+    if (!this.state.listingLoaded) return null;
     return (
       <div className="edit-listing-page">
         <h1 className="edit-listing-page__title">Edit Listing</h1>
@@ -183,10 +134,10 @@ export class EditListingPage extends Component {
             alt="Add photos"
           />
           <div className="edit-listing-page__upload-images">
-            {this.state.images.map((image, i) => {
+            {this.state.listing.images.map((image, i) => {
               return (
                 <figure key={uuidv4()} className="edit-listing-page__upload-figure">
-                  <img className="edit-listing-page__upload-image" src={this.state.images[i]} alt="image-1" />
+                  <img className="edit-listing-page__upload-image" src={this.state.listing.images[i]} alt="image-1" />
                   <img
                     data-index={i}
                     onClick={this.handleDeleteImage}
@@ -199,86 +150,12 @@ export class EditListingPage extends Component {
             })}
           </div>
         </div>
-
-        <div className="edit-listing-page__container-bottom">
-          <div className="edit-listing-page__container-bottom-left">
-            <InputField
-              name="title"
-              label="Title *"
-              value={this.state.title}
-              placeholder="Enter Title..."
-              onChange={this.handleChange}
-              error={this.state.titleError}
-            />
-            <div className="edit-listing-page__currency-input">
-              <div className="edit-listing-page__amount">
-                <InputField
-                  name="price"
-                  label="Price *"
-                  value={this.state.price}
-                  placeholder="Enter Price..."
-                  onChange={this.handleChange}
-                  error={this.state.priceError}
-                />
-              </div>
-              <div className="edit-listing-page__currency">
-                <DropDownField
-                  name="listCurrency"
-                  label="Currency"
-                  options={['ETH', 'BTC']}
-                  value={this.state.listCurrency}
-                  onChange={this.handleChange}
-                  error=""
-                />
-              </div>
-            </div>
-            <DropDownField
-              name="category"
-              label="Category"
-              options={['Miscellaneous', 'Home', 'Clothing', 'Electronics', 'Hobbies', 'Entertainment', 'Sporting']}
-              value={this.state.category}
-              onChange={this.handleChange}
-              error=""
-            />
-            <InputField
-              name="condition"
-              label="Condition"
-              value={this.state.condition}
-              placeholder="Enter Condition..."
-              onChange={this.handleChange}
-              error=""
-            />
-          </div>
-          <div className="edit-listing-page__container-bottom-right">
-            <InputField
-              name="location"
-              label="Location"
-              value={this.state.location}
-              placeholder="Enter Postal Code..."
-              onChange={this.handleChange}
-              error=""
-            />
-            <TextArea
-              name="description"
-              label="Description *"
-              value={this.state.description}
-              placeholder="Enter Description..."
-              onChange={this.handleChange}
-              error={this.state.descriptionError}
-            />
-            <div className="edit-listing-page__buttons">
-              <div className="edit-listing-page__button-cancel">
-                <ButtonSecondary label="Cancel" handleClick={this.handleCancel} />
-              </div>
-              <div className="edit-listing-page__button-cancel">
-                <ButtonDanger label="Delete" handleClick={this.handleDeleteListing} />
-              </div>
-              <div className="edit-listing-page__button-create">
-                <ButtonPrimary label="Submit" handleClick={this.handleEditListing} />
-              </div>
-            </div>
-          </div>
-        </div>
+        <ListingForm
+          listing={this.state.listing}
+          handleCancel={this.handleCancel}
+          handleDelete={this.handleDeleteListing}
+          handleSubmit={this.handleEditListing}
+        />
       </div>
     );
   }
