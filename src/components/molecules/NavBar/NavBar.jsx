39d4e8eea628e2dc0cd3e5280383857c097ab503
Jacob Hildebrandt <jake@hildy.ca>
2021-05-05 14:34:55 -0700

    Added Eth Logo to Navbar

--- src/components/molecules/NavBar/NavBar.jsx
+++ src/components/molecules/NavBar/NavBar.jsx
@@ -1,21 +1,19 @@
-import React from "react";
-import "./NavBar.scss";
-import { Link } from "react-router-dom";
-import ProfileIcon from "./../../../assets/icons/account_circle_black_24dp.svg";
+import React from 'react';
+import './NavBar.scss';
+import { Link } from 'react-router-dom';
+import ProfileIcon from './../../../assets/icons/account_circle_black_24dp.svg';
+import EthLogo from './../../../assets/icons/eth-logo.png';
 
 function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar__container">
         <Link to="/browse" className="navbar__browse-link">
+          <img src={EthLogo} alt="Ethermarket Logo" className="navbar__logo" />
           <h2 className="navbar__home-text">EtherMarket</h2>
         </Link>
         <Link to="/home" className="navbar__home-link">
-          <img
-            src={ProfileIcon}
-            alt="Navbar Profile Icon"
-            className="navbar__profile-icon"
-          />
+          <img src={ProfileIcon} alt="Navbar Profile Icon" className="navbar__profile-icon" />
         </Link>
       </div>
     </nav>
