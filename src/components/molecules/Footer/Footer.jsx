2c30089e274dc7c904646b6d50b0ef725798267d
Jacob Hildebrandt <jake@hildy.ca>
2021-05-11 10:16:46 -0700

    Added Icon Links to Footer

--- src/components/molecules/Footer/Footer.jsx
+++ src/components/molecules/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.scss';
+import IconBriefcase from './../../atoms/IconBriefcase/IconBriefcase';
+import IconGithub from './../../atoms/IconGithub/IconGithub';
+import IconLinkedin from './../../atoms/IconLinkedin/IconLinkedin';
 
 function Footer() {
   return (
@@ -13,6 +16,17 @@ function Footer() {
         <Link to={'#'} className="footer__link">
           Investors
         </Link>
+      </div>
+      <div className="footer__icon-links">
+        <a href="http://hildy.ca/" className="footer__icon-links--1">
+          <IconBriefcase fill={'#fff'} />
+        </a>
+        <a href="https://github.com/JakeHildy" className="footer__icon-links--2">
+          <IconGithub fill={'#fff'} />
+        </a>
+        <a href="https://www.linkedin.com/in/jacob-hildebrandt-web-dev/" className="footer__icon-links--3">
+          <IconLinkedin fill={'#fff'} />
+        </a>
       </div>
     </div>
   );
